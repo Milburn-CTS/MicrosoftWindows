bc085ec7b4e44ed626f6fa822ecb7a2581261526
MilburnGomes <milburn.gomes@continentaltechsolutions.com>
2019-02-19 14:15:56 +0530

    Basic Authentication in WEB API using C# Initail Commit 19.02.2019

---
+++ WebAPIBasicAuth/WebAPIBasicAuth/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIBasicAuth.Attributes;
+
+namespace WebAPIBasicAuth.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        // GET api/Default
+        public string Get()
+        {
+            //System.Diagnostics.Process.Start("https://www.google.com/");
+            return "Hello! Inside Get";
+        }
+
+        // POST api/Default
+        [BasicAuthentication]
+        public string Post()
+        {
+            //System.Diagnostics.Process.Start("https://www.google.com/");
+            return "Hello! Insie Post";
+        }
+
+    }
+}
